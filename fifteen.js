e15240ab845eedc5986bc90df2e6516c3bc26fba
brittneychin <b.chin96@live.com>
2016-11-03 08:33:57 -0500

    second commit

--- fifteen.js
+++ fifteen.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
 	var puzzle = $("#puzzlearea div");
-	for (var i = puzzle.length - 1; i >= 0; i--) {
+	var x = 0;
+	var y = 0;
+	var countRow = 0;
+	for (var i = 0; i < puzzle.length;i++) {
 		puzzle[i].setAttribute("class","puzzlepiece");
 		puzzle[i].style.backgroundRepeat= "no-repeat";
 		puzzle[i].style.backgroundSize = "400px 400px";
 		puzzle[i].style.float = "left";
 		puzzle[i].style.position = "static";
-		puzzle[i].style.backgroundPosition = "-100px 0px"
+		puzzle[i].style.backgroundPosition = "-" + x + "px -" + y + "px";
+		countRow++;
+		if(countRow==4){
+			y+=96; 
+			x = 0;
+			countRow = 0;
+		}
+		else{
+			x+=96;
+		}
     }
+
+    var	blankTile =  document.createElement("div");
+    blankTile.style.border = "2px solid black";
+    $("#puzzlearea").append(blankTile);
+
 });
